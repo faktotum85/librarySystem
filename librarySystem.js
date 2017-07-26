5c34a80cfebaef086ec61b051211a5372d613275
Simon Weller <s.meierbeck@gmail.com>
2017-07-26 14:34:05 +0200

    refined to accept dependencies

--- librarySystem.js
+++ librarySystem.js
@@ -1,10 +1,23 @@
 (function () {
   var libraryStorage = {};
 
-  function librarySystem(libraryName, callback) {
+  function librarySystem(libraryName, dependencies, callback) {
+    var resolvedDependencies = [];
+    // storing to librarySystem
     if (arguments.length > 1) {
-      libraryStorage[libraryName] = callback();
+      if (dependencies.length > 0) {
+        // recursively resolve dependencies
+        resolvedDependencies = dependencies.map(function resolve(dependency) {
+          return librarySystem(dependency);
+        });
+        // pass in resolved dependencies
+        libraryStorage[libraryName] = callback.apply(this, resolvedDependencies);
+      } else {
+        // no dependencies, just store result of callback
+        libraryStorage[libraryName] = callback();
+      }
     }
+    // always return result; only thing that runs if only one argument supplied
     return libraryStorage[libraryName];
   }
   window.librarySystem = librarySystem;
