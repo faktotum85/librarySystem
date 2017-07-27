f47aa0be56dbed60594cc5f8bc4dd2ef70d3e845
Simon Weller <s.meierbeck@gmail.com>
2017-07-27 14:33:48 +0200

    v2 allowing for libraries being created out of order

--- librarySystem.js
+++ librarySystem.js
@@ -3,22 +3,94 @@
 
   function librarySystem(libraryName, dependencies, callback) {
     var resolvedDependencies = [];
-    // storing to librarySystem
-    if (arguments.length > 1) {
-      if (dependencies.length > 0) {
-        // recursively resolve dependencies
-        resolvedDependencies = dependencies.map(function resolve(dependency) {
-          return librarySystem(dependency);
-        });
-        // pass in resolved dependencies
-        libraryStorage[libraryName] = callback.apply(this, resolvedDependencies);
-      } else {
-        // no dependencies, just store result of callback
-        libraryStorage[libraryName] = callback();
+    var resolvedValue;
+    var storedLibrary = libraryStorage[libraryName];
+    if (arguments.length > 1) { // extra argunments - storing to librarySystem
+      libraryStorage[libraryName] = {
+        resolved: false,
+        callback: callback,
+        dependencies: dependencies
+      };
+    } else { // single argument passed in - return result
+      // check if value is already cached
+      if (storedLibrary.resolved === true) {
+        return storedLibrary.value;
       }
+      // not been called yet -> resolve dependencies and value
+      resolvedDependencies = storedLibrary.dependencies.map(function resolve(dependency) {
+        return librarySystem(dependency);
+      });
+      resolvedValue = storedLibrary.callback.apply(this, resolvedDependencies);
+      // cache and return; need to assign directly to libraryStorage to overwrite
+      libraryStorage[libraryName] = {
+        resolved: true,
+        value: resolvedValue
+      };
+      return resolvedValue;
     }
-    // always return result; only thing that runs if only one argument supplied
-    return libraryStorage[libraryName];
   }
   window.librarySystem = librarySystem;
 }());
+
+tests({
+  'It should store and load libraries in a single global variable': function () {
+    librarySystem('app', [], function () {
+      return 'app';
+    });
+    eq(librarySystem('app'), 'app');
+  },
+  'It should accept dependencies': function () {
+    librarySystem('dependency', [], function () {
+      return 'loaded dependency';
+    });
+    librarySystem('app', ['dependency'], function (dependency) {
+      return 'app with ' + dependency;
+    });
+    eq(librarySystem('app'), 'app with loaded dependency');
+  },
+  'It should work with multiple dependencies': function () {
+    librarySystem('name', [], function () {
+      return 'Gordon';
+    });
+    librarySystem('company', [], function () {
+      return 'Watch and Code';
+    });
+    librarySystem('workBlurb', ['name', 'company'], function (name, company) {
+      return name + ' works at ' + company;
+    });
+    eq(librarySystem('workBlurb'), 'Gordon works at Watch and Code');
+  },
+  'It should allow libraries to be created out of order': function () {
+    // reset librarySystem to avoid previous test influencing results
+    librarySystem('workBlurb', [], function () { return undefined; });
+    librarySystem('name', [], function () { return undefined; });
+    librarySystem('company', [], function () { return undefined; });
+    // run test
+    librarySystem('workBlurb', ['name', 'company'], function (name, company) {
+      return name + ' works at ' + company;
+    });
+    librarySystem('name', [], function () {
+      return 'Gordon';
+    });
+    librarySystem('company', [], function () {
+      return 'Watch and Code';
+    });
+    eq(librarySystem('workBlurb'), 'Gordon works at Watch and Code');
+  },
+  'It should only run the callback function for each library once': function () {
+    var callbackCount = 0;
+    librarySystem('dependedOn', [], function () {
+      callbackCount += 1;
+      return 'depended on';
+    });
+    librarySystem('iAm', ['dependedOn'], function (dependedOn) {
+      return 'I am ' + dependedOn;
+    });
+    librarySystem('youAre', ['dependedOn'], function (dependedOn) {
+      return 'You Are ' + dependedOn;
+    });
+    librarySystem('iAm');
+    librarySystem('youAre');
+    eq(callbackCount, 1);
+  }
+});
